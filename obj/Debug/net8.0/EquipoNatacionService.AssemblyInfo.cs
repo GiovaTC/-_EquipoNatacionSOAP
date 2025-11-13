7f4f6fbebdfc9580081468658a0791f856f342e0
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-13 10:22:27 -0500

    commit- DatosPrueba_EquipoNatacion .

--- obj/Debug/net8.0/EquipoNatacionService.AssemblyInfo.cs
+++ obj/Debug/net8.0/EquipoNatacionService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EquipoNatacionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035d4cdf851b70c92092b41c6462365ebd441d80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+137cb93e0b24b155b9162719e1828e7e08644e21")]
 [assembly: System.Reflection.AssemblyProductAttribute("EquipoNatacionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EquipoNatacionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
