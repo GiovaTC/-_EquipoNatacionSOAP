dbdbe57f6540d13ebb32ac7bec5715a6745c3946
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-13 09:20:45 -0500

    commit- ScriptTablasOracle :.

--- obj/Debug/net8.0/EquipoNatacionService.AssemblyInfo.cs
+++ obj/Debug/net8.0/EquipoNatacionService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EquipoNatacionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5271995d5b64894a6962a034a071efeba146ab6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f55a269edad27bb6f535e6ff4447baaecd77334")]
 [assembly: System.Reflection.AssemblyProductAttribute("EquipoNatacionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EquipoNatacionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
