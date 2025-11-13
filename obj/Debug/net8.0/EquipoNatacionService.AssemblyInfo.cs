0d746570ea1be42d978e8ec52fcc7656523ba688
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-13 11:21:59 -0500

    commit- EquipoService-soapui-project .

--- obj/Debug/net8.0/EquipoNatacionService.AssemblyInfo.cs
+++ obj/Debug/net8.0/EquipoNatacionService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EquipoNatacionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a0d4c69a97ce3163626bad408acc22aef26cae8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faad2e8c43c3d278164a5248c3b7447964395b84")]
 [assembly: System.Reflection.AssemblyProductAttribute("EquipoNatacionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EquipoNatacionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
